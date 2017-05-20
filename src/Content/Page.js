9897206ba2042d1036ce98c6a6afa0c1490fb22e
ztaira14 <ztaira14@punahou.edu>
2017-05-20 12:06:45 -0400

    Page can now format stuff.

--- src/Content/Page.js
+++ src/Content/Page.js
@@ -17,11 +17,54 @@ class Page extends Component {
     let _this = this;
     this.xhp = new XMLHttpRequest();
     this.xhp.onload = function() {
-      let data = this.responseText;
-      _this.setState({text: data});
+      _this.setState({text: _this.ProcessText(this.responseText)});
     };
     this.xhp.open("GET", url);
     this.xhp.send();
+  }
+
+  ProcessText(text) {
+    let text_list = text.split('\n');
+    let div_list = [];
+    let current_paragraph = '';
+    let current_line = '';
+    let num_hashtags = '';
+    for (var line=0; line < text_list.length; line++) {
+      current_line = text_list[line];
+      num_hashtags = (current_line.match(/#/g) || []).length;
+      if (num_hashtags > 0) {
+        div_list.push(this.ReturnBoldText(current_line, num_hashtags, line));
+      }
+      else if (current_line !== '') {
+        current_paragraph += ' ' + current_line;
+      }
+      else {
+        div_list.push(<p key={line}>{current_paragraph}</p>);
+        current_paragraph = '';
+      }
+    }
+    return div_list;
+  }
+
+  ReturnBoldText(text, num_hashtags, key) {
+    if (num_hashtags === 1) {
+      return <h1 key={key}>{text.slice(text.indexOf(' ') + 1)}</h1>;
+    }
+    else if (num_hashtags === 2) {
+      return <h2 key={key}>{text.slice(text.indexOf(' ') + 1)}</h2>;
+    }
+    else if (num_hashtags === 3) {
+      return <h3 key={key}>{text.slice(text.indexOf(' ') + 1)}</h3>;
+    }
+    else if (num_hashtags === 4) {
+      return <h4 key={key}>{text.slice(text.indexOf(' ') + 1)}</h4>;
+    }
+    else if (num_hashtags === 5) {
+      return <h5 key={key}>{text.slice(text.indexOf(' ') + 1)}</h5>;
+    }
+    else if (num_hashtags === 6) {
+      return <h6 key={key}>{text.slice(text.indexOf(' ') + 1)}</h6>;
+    }
   }
 
   componentWillUnmount() {
