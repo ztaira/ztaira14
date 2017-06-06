1b6f4adc0b0ea6679dfca35af6e0df39949a55eb
Zachary Taira <zach.taira@gmail.com>
2017-06-06 17:13:02 -0400

    added a snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const app = renderer.create(
+    <App />
+  );
+  let app_json = app.toJSON();
+  expect(app_json).toMatchSnapshot();
 });
