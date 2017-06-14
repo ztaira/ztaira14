688b83f55f3d937e45f550ca559625d1d01f2441
Zachary Taira <zach.taira@gmail.com>
2017-06-13 20:15:09 -0400

    fixed safari issue - it was flex-basis.

--- copy_to_build.sh
+++ copy_to_build.sh
@@ -14,8 +14,10 @@ case ${answer} in
     echo 'copying over to ztaira14.github.io'
     cp build/index.html ../ztaira14.github.io
     cp build/asset-manifest.json ../ztaira14.github.io
-    cp -r build/static ../ztaira14.github.io
+    cp -R build/static ../ztaira14.github.io
     echo 'modifying package.json back to normal...'
     sed -i -e "s:ztaira14.github.io:ztaira14.github.io/ztaira14:" package.json && cat package.json
+    echo 'building the old one again...'
+    npm run build
     echo 'done!'
 esac
