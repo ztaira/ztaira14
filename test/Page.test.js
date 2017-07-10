c7ea5ab5f4a7f81dbda2ba2ef9c6a25b351d14ac
ztaira14 <ztaira14@punahou.edu>
2017-07-10 00:11:12 -0400

    added page tests, modified projectsbackup test.

---
+++ test/Page.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+import renderer from 'react-test-renderer';
+import Page from '../src/Content/Page.jsx';
+
+beforeEach(function() {
+  this.project = ReactDOM.render(
+    <Page />,
+    document.body.appendChild(document.createElement('div'))
+  );
+});
+
+it('has a working function to process text in basic markdown format', function() {
+  // all lines with a hash in front of it should be substituted for header tags
+  let example_text = '# h1\n## h2\n### h3\n#### h4\n##### h5\n###### h6\n\n';
+  // it should split paragraph text on two or more consecutive return statements
+  example_text += 'paragraph 1\nparagraph1 continued\n\nparagraph2\n\n';
+
+  // the resulting list should contain the following:
+  // <h1>h1</h1>
+  // <h2>h2</h2>
+  // <h3>h3</h3>
+  // <h4>h4</h4>
+  // <h5>h5</h5>
+  // <h6>h6</h6>
+
+  this.project.setState({text: this.project.ProcessText(example_text)});
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[0])).toBe('<h1>h1</h1>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[1])).toBe('<h2>h2</h2>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[2])).toBe('<h3>h3</h3>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[3])).toBe('<h4>h4</h4>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[4])).toBe('<h5>h5</h5>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[5])).toBe('<h6>h6</h6>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[6])).toBe('<p></p>');
+  // todo: fix the spaces
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[7])).toBe('<p> paragraph 1 paragraph1 continued</p>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[8])).toBe('<p> paragraph2</p>');
+  expect(ReactDOMServer.renderToStaticMarkup(this.project.state.text[9])).toBe('<p></p>');
+  expect(this.project.state.text.length).toBe(10);
+});
--- test/ProjectsBackup.test.js
+++ test/ProjectsBackup.test.js
@@ -1,35 +1,5 @@
 import ProjectsBackup from '../src/Content/ProjectsBackup.js';
 
-it('matches the response from api.github.com/users/ztaira14/repos', done => {
-  let _this = this;
-  let xhp = new XMLHttpRequest();
-  xhp.onload = function() {
-    let response = JSON.parse(this.responseText);
-    // console.log(this.responseText);
-    console.log(response.length);
-    console.log(ProjectsBackup.length);
-    expect(response.length).toBe(ProjectsBackup.length);
-    done();
-    // for (counter = 0; counter < response.length; counter=counter+1) {
-      // console.log(i);
-      // let responseObject = response[i];
-      // let backupObject = ProjectsBackup.filter(item => {
-        // return item.name === responseObject.name;
-      // });
-      // console.log(backupObject);
-      // expect(responseObject.name).toBe(backupObject.name);
-      // expect(responseObject.description).toBe(backupObject.description);
-      // expect(responseObject.language).toBe(backupObject.language);
-      // expect(responseObject.created_date).toBe(backupObject.created_date);
-    // };
-    // done();
-  };
-  xhp.onerror = function() {
-    expect(true).toBe(false);
-    console.log('it errored out');
-    done();
-  };
-  xhp.open('GET', 'https://api.github.com/users/ztaira14/repos');
-  console.log('sending xhp');
-  xhp.send();
+it('has the same amount of projects', function() {
+  expect(ProjectsBackup.length).toBe(32);
 });
