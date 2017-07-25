04d0f0cdc9f3d749b17c3fca77596f6b18c6e3e6
ztaira14 <ztaira14@punahou.edu>
2017-07-25 07:15:07 -0400

    this'll be cool, I promise

---
+++ src/Content/Modal.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Modal.css';
+
+class Modal extends Component {
+  render() {
+    return (
+      <div className="Modal"></div>
+    );
+  }
+}
+
+export default Modal;
